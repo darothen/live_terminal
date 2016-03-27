0883a62ef7cc36ca2fcf184d8e70af6af1f43ceb
Daniel Rothenberg <darothen@mit.edu>
2016-03-27 16:35:53 -0400

    Updating labeling and styling on plot

--- static/js/obs_plot.js
+++ static/js/obs_plot.js
@@ -1,4 +1,4 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
+var margin = {top: 20, right: 20, bottom: 50, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -16,19 +16,10 @@ var xAxis = d3.svg.axis()
 
 // Set up y-axis
 var y = d3.scale.linear()
-    .range([0, height]);
+    .range([height, 0]);
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
-
-// Set up line chart
-var line = d3.svg.line()
-    .x(function(d) {
-        return x(d.date);
-    })
-    .y(function(d) {
-        return y(d.temp);
-    });
 
 // Set up plotting area
 var svg = d3.select("#graph").append("svg")
@@ -37,6 +28,13 @@ var svg = d3.select("#graph").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," +
                                       margin.top + ")");
+
+ var lineGen = function(data, key) {
+    var line = d3.svg.line()
+                .x(function(d) { return x(d.date); })
+                .y(function(d) { return y(d[key]); });
+    return line(data);
+};
 
 // Load data and map it to plot
 d3.csv('data/test_obs.csv', get_temps, function(error, data) {
@@ -48,11 +46,11 @@ d3.csv('data/test_obs.csv', get_temps, function(error, data) {
             return d.date;
         })
     );
-    y.domain(
-        d3.extent(data, function(d) {
-            return d.temp;
-        })
-    );
+    var y_pad = 2.5;
+    y.domain([
+        d3.min(data, function(d) { return Math.min(d.temp, d.dwpt) - y_pad; }),
+        d3.max(data, function(d) { return Math.max(d.temp, d.dwpt) + y_pad; }),
+    ]);
 
     // Construct actual plot line elements
     // 1) x-axis
@@ -60,15 +58,64 @@ d3.csv('data/test_obs.csv', get_temps, function(error, data) {
         .attr("class", "x axis") // CSS selector
         .attr("transform", "translate(0," + height + ")") // move to bottom of plot
         .call(xAxis); // add the x-axis
+    svg.append("text")
+        .attr("class", "axislabel")
+        .attr("x", width/2)
+        .attr("y", height + margin.bottom)
+        .text("Time");
+
     // 2) y-axis
     svg.append("g")
         .attr("class", "y axis") // CSS selector
         .call(yAxis);
+    svg.append("text")
+        .attr("class", "axislabel")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .text("Temperature / Dewpoint (F)");
+
     // 3) temperature plot
     svg.append("path")
-        .datum(data) // Bind data to plot path elements
-        .attr("class", "line") // CSS selector
-        .attr("d", line);
+        // .data(data) // Bind data to plot path elements
+        .attr("class", "temp line") // CSS selector
+        .style("fill", "none")
+        .attr("d", lineGen(data, 'temp'));
+    // 4) Temperature glyphs
+    svg.append("g")
+        .attr("class", "temp dots") // CSS selector
+      .selectAll("path")
+        .data(data)
+      .enter().append("path")
+        .attr("transform", function(d) {
+            return "translate(" + x(d.date) + "," + y(d.temp) + ")";
+        })
+        .attr("d",
+            d3.svg.symbol()
+                .size(50)
+                .type("cross")
+        );
+    // 5) Dewpoint plot
+    svg.append("path")
+        // .datum(data) // Bind data to plot path elements
+        .attr("class", "dwpt line") // CSS selector
+        .style("fill", "none")
+        .attr("d", lineGen(data, 'dwpt'));
+    // 6) Dewpoint glyphs
+    svg.append("g")
+        .attr("class", "dwpt dots") // CSS selector
+      .selectAll("path")
+        .data(data)
+      .enter().append("path")
+        .attr("transform", function(d) {
+            return "translate(" + x(d.date) + "," + y(d.dwpt) + ")";
+        })
+        .attr("d",
+            d3.svg.symbol()
+                .size(50)
+                .type("cross")
+        );
 
 });
 
